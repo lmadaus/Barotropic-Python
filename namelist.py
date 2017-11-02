b91c560cbc931fc88c7f5804fef8117136bd805e
Nicholas Weber <njweber2@atmos.washington.edu>
2017-11-02 16:37:26 -0700

    Restructured code to be class-based

--- namelist.py
+++ namelist.py
@@ -4,17 +4,16 @@ This module contains the variables used to run barotropic_spectral.py
 """
     
 # Set parameters
-M = 42
+M = None                   # Truncation (if None, defaults to # latitudes)
 Re = 6378100.              # Radius of earth (m)
 omega = 7.292E-5           # Earth's angular momentum (s^-1)
+g = 9.81                   # Gravitational acceleration (m s^-2)
 dt = 900.                  # Timestep (seconds)
-ntimes = 480               # Number of time steps to integrate
+ntimes = 960               # Number of time steps to integrate
 output_freq = 6            # Frequency of output plots in hours
-use_hyper = False           # Whether or not to apply hyperdiffusion
-damping_order = 1          # Order of dampening
+use_hyper = True           # Whether or not to apply hyperdiffusion
 nu = 1E-4                  # Dampening coefficient for hyperdiffusion
-restart = None             # Filename of restart file to load
 plot_output = True         # Whether or not to plot the output
-fourier_inc = 1            #
-r = 0.0                    # (was 0.2)
+fourier_inc = 1            # Fourier increment for computing dampening eddy sponge
+r = 0.2                    # Coefficient for Robert Filter
 figdir = '/home/disk/p/njweber2/research/subseasonal/barotropic/Barotropic-Python/figures'
